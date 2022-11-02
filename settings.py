df9fd4ce6b5ba005abe3f751fc716689878338c4
Archermysteri <ratibor200805@gmail.com>
2022-11-02 23:43:48 +0200

    Add README.md and Setting

--- settings.py
+++ settings.py
@@ -4,19 +4,21 @@ config = ConfigParser()
 config.read("config.ini")
 
 def set_basic_settings():
-
+    changes=False
 
     if config["USER"]["language"]=="":
         set_language()
-
+        changes=True
     if config["USER"]["api_hash"]=="" or config["USER"]["api_id"]=="":
+        changes=True
         api_id=str(input('>api_id:'))
         api_hash=str(input('>api_hash:'))
         config["USER"]["api_hash"]=api_hash
         config["USER"]["api_id"]=api_id
-    with open("config.ini", 'w') as configfile:
-        config.write(configfile)
-    print(config[config["USER"]["language"]]["settings_basic_end"])
+    if changes==True:
+        with open("config.ini", 'w') as configfile:
+            config.write(configfile)
+        print(config[config["USER"]["language"]]["settings_basic_end"])
 
 def set_language():
     Languages = ""
@@ -47,26 +49,4 @@ def Delete_session():
         os.remove("my_account.session")
     except:
         pass
-def start():
-    if config["USER"]["api_hash"]=="" or config["USER"]["api_id"]=="" or config["USER"]["language"]=="" :
-        set_basic_settings()
-    text = ""
-    for i in config[config["USER"]["language"]]["settings_start"]:
-        if i == 'Ø':
-            text += '\n'
-        else:
-            text += i
-    while True:
-        print("\n",text)
-        comand=str(input(">"))
-        print()
-        if comand=="del":
-            Delete_session()
-            print(config[config["USER"]["language"]]["del_end"])
-        elif comand=="language":
-            set_language()
-            print(config[config["USER"]["language"]]["set_languages_end"])
-        elif comand=="end":
-            break
-        else:
-            print(config[config["USER"]["language"]]["WTF"])
+
